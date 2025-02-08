9365b92df7e099a489cd6f18e7e0f7963e29d47a
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2025-02-09 00:42:30 +0900

    chore: 페이지 헤더 추가

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Blog',
+      pageId: '15869bf8a344800eb94debaf46c50120'
+    },
+    {
+      title: 'Portfolio',
+      pageId: '42653a19e5d44660a9ebc7268d3c0bc0'
+    }
+  ]
 })
